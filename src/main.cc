3f3066f1d86143ac64ceade0f9829fc050366fdb
Borroot <borroot@live.nl>
2021-06-13 15:50:50 +0200

    Add SFML

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,22 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
 }
